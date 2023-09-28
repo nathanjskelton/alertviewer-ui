fc639e38ed8054acaee89bf414494cadf428308c
Nathan Skelton njskelt <njskelt@uwe.nsa.gov>
2023-09-28 21:53:03 +0000

    many changes

---
+++ src/views/Silences.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+    <SilencesList  />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SilencesList from "@/components/SilencesList.vue";
+
+export default {
+  name: "GM Silences",
+  components: {
+    SilencesList,
+  }
+};
+</script>
